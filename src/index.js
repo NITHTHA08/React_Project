011489005b2d329b093e7b9a8423939d1310c4c8
mac <mac@macs-MacBook-Pro.local>
2024-08-28 17:14:12 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Text from './components/nav';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
